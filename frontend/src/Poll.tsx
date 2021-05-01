2aaace2c92c45deb9483b6390c1ff10eb7639fde
Zenios Zeniou <mrzenioszeniou@gmail.com>
2021-05-01 22:30:51 +0200

    Getting there

--- frontend/src/Poll.tsx
+++ frontend/src/Poll.tsx
@@ -8,8 +8,35 @@ interface RouteInterface {
 
 interface Props extends RouteComponentProps<RouteInterface> {}
 
+enum Stage {
+  Start,
+  Vote,
+  Results,
+}
+
+enum Vote {
+  Zero = 0.0,
+  Half = 0.5,
+  One = 1.0,
+  Two = 2.0,
+  Three = 3.0,
+  Five = 5.0,
+  Eight = 8.0,
+  Thirteen = 13.0,
+}
+
+interface PollData {
+  title: string;
+  desc: string;
+  votes: Map<string, Vote>;
+}
+
 interface State {
   poll: string;
+  stage: Stage;
+  email: string;
+  vote: Vote;
+  pollData: PollData | null;
 }
 
 class Poll extends React.Component<Props, State>  {
@@ -18,13 +45,41 @@ class Poll extends React.Component<Props, State>  {
     super(props);
     this.state = {
       poll: props.match.params.poll,
+      stage: Stage.Start,
+      email: "",
+      vote: Vote.Zero,
+      pollData: null,
     };
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:8000/poll/' + this.state.poll;
+    const requestOptions = {
+      method: 'GET',
+      header: {'Content-Type': 'application/json'},
+    };
+    
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(body => this.setState({pollData: body}));
   }
   
   render() {
     return (
-      <div className="base centered">
+      <div className="base centered" style={{justifyContent: "normal"}}>
+
+
+        <div className="poll-details">
+          <div className="poll-title">{ this.state.pollData === null ?  null : this.state.pollData.title }</div>
+          <div className="poll-desc">{ this.state.pollData === null ?  null : this.state.pollData.desc }</div>
+        </div>
+
+
+
         Poll ({this.state.poll}) shit goes here..
+
+
+
       </div>
     );
   }
